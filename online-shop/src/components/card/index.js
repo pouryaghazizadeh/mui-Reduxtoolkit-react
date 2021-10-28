5332515b49315cd1f2d5c1f724199669150dbe23
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-10-28 22:33:38 +0200

    make home card component

---
+++ online-shop/src/components/card/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div>
+            jbf
+        </div>
+    )
+}
+
+export default Card
---
+++ online-shop/src/components/navbar/view.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components"
+
+
+export const Nav = styled.nav`
+  width: 100vw;
+  height: 80px;
+  background: #1e1f25;
+`;
+
+export const TitleNav = styled(Link)`
+  font-size: xx-large;
+  text-decoration: none;
+  color: #fefefe;
+`;
+
+
+export const ContainerIcon = styled.section`
+
+`
+export const BoxIconShope = styled.div`
+font-size: 35px;
+
+`;
+
+
+export const NumberProduct = styled.span``
