1d6817ebe1569eb0149303e786a254b91f1645e3
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-10-30 11:16:28 +0200

    fix bug

---
+++ online-shop/frontend/src/redux/features/productSlice.js
@@ -0,0 +1,15 @@
+import { createSlice,createAsyncThunk  } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+  status: null,
+};
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+});
+
+
+export default productSlice.reducer
