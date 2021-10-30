66bb78ff531cc942bf6f3e1f9b5ae5402b58783f
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-10-30 13:50:23 +0200

    write redux

--- online-shop/backend/index.js
+++ online-shop/backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-
+const products = require("./products")
 const app = express()
 
 
@@ -13,6 +13,11 @@ app.get("/",(req,res)=>{
 res.send("welcome to home page")
 })
 
+app.get("/products", (req, res) => {
+  res.send(products );
+});
+
+
 
 const port = process.env.PORT || 5000
-app.listen(port, console.log("server runing"));
+app.listen(port, console.log("server runing port 5000"));
---
+++ online-shop/backend/products.js
@@ -0,0 +1,26 @@
+const products = [
+  {
+    id: 1,
+    name: "iphone12",
+    des: "hi i am iphone 12",
+    price: 999,
+    img: "https://images.unsplash.com/photo-1555375771-14b2a63968a9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aXBob25lJTIwMTB8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 2,
+    name: "iphone12 pro",
+    des: "hi i am iphone 12 pro",
+    price: 1000,
+    img: "https://images.unsplash.com/photo-1589798312995-8428138bb1ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGlwaG9uZSUyMDEwfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 3,
+    name: "iphone12 pro max",
+    des: "hi i am iphone 12 pro max",
+    price: 777,
+    img: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGlwaG9uZSUyMDEwfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+];
+
+
+module.exports = products
